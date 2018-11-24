8f6a576c1bf8a8402b549d08028260c83b73c9d5
jmugyenyi <jmugyenyi@169.254.167.69>
2018-11-24 21:39:51 +0200

    AI-3.2.1 <jmugyenyi@jmugyenyi Create project.default.xml, Singleton.java, Enum.java, sqliteSettings.xml, laf.xml, Interface.java, Class.java, jdk.table.xml, Author.java, Default.xml, path.macros.xml, androidStudioFirstRun.xml, find.xml, ui.lnf.xml, File Header.java, editor.codeinsight.xml, colors.scheme.xml, debugger.xml, AnnotationType.java

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+
+/**
+ * * @author Joel Mugyenyi
+ *
+ * Andrew ID: jmugyeny
+ *
+ * On my honor, as a Carnegie-Mellon Africa student, I have neither given nor
+ * received unauthorized assistance on this work.!
+ */
