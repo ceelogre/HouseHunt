b7ffbd5979fc8ec97e5881d9eee7a7f142119be2
mugyenyij <mugyenyij@gmail.com>
2018-11-28 21:11:01 +0200

    Adding RecyclerView to project

--- app/src/main/java/com/example/jmugyenyi/mychat/MainActivity.java
+++ app/src/main/java/com/example/jmugyenyi/mychat/MainActivity.java
@@ -29,7 +29,6 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-
     protected static final String TAG = "MainActivity";
 
     private Toolbar mToolbar;
@@ -42,18 +41,13 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference databaseReference;
     private SeekerTabsAdapter seekers;
 
-    DatabaseHelperClass dh ;
 
     final User myUser = new User();
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        dh = new DatabaseHelperClass(this);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         currentUser = mFirebaseAuth.getCurrentUser();
@@ -67,39 +61,17 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("HouseHunt");
 
         myViewPager = findViewById(R.id.main_tabs_pager);
-       // myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
 
+         String myStatus = "House head";//user.getUserStatus();
 
-
-
-
-        // user.setUserStatus("seeker");
-        //String stats = getIntent().getExtras().getString("status");
-         String myStatus = "seeker";//user.getUserStatus();
-
-//        if (getIntent().getExtras().getString("status")== null)
-//        {
-//            myStatus ="House head";
-//        }else
-//            myStatus = getIntent().getExtras().getString("status");
-
-        //Log.d(TAG, "onCreate stats: "+stats);
-        //Log.d("userStatus",  myStatus);
         if(myStatus == "seeker")
         {
             seekers = new SeekerTabsAdapter(getSupportFragmentManager());
-            //myViewPager.setAdapter(myTabsAccessorAdapter);
-            myViewPager.setAdapter(seekers);
         }
         else if (myStatus == "House head")
         {
             myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
-            myViewPager.setAdapter(myTabsAccessorAdapter);
         }
-
-
-        myTabLayout = findViewById(R.id.main_tabs);
-        myTabLayout.setupWithViewPager(myViewPager);
     }
 
     @Override
@@ -146,7 +118,6 @@ public class MainActivity extends AppCompatActivity {
         builder.setTitle("Enter group name: ");
 
         final EditText groupnameField = new EditText(MainActivity.this);
-        //groupnameField.setHint("e.g The fuck?");
         builder.setView(groupnameField);
 
         builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
@@ -224,13 +195,7 @@ public class MainActivity extends AppCompatActivity {
         User user = RetrieveUserInfo();
 
         Log.d(TAG, "VerifyUser: Status: "+user.getUserStatus());
-//        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
-//        myViewPager.setAdapter(myTabsAccessorAdapter);
-//
-//
-//
-//        myTabLayout = findViewById(R.id.main_tabs);
-//        myTabLayout.setupWithViewPager(myViewPager);
+
     }
 
     private void SendUserToLoginActivity() {
@@ -258,9 +223,6 @@ public class MainActivity extends AppCompatActivity {
 
     private User  RetrieveUserInfo( ) {
 
-        String status="";
-        //final User myUser = new User();
-        //currentUserID = mFirebaseAuth.getCurrentUser().getUid();
         Log.d(TAG, "RetrieveUserInfo: "+ currentUserId);
         databaseReference.child("Users").child(currentUserId).addValueEventListener(new ValueEventListener() {
             @Override
@@ -269,12 +231,8 @@ public class MainActivity extends AppCompatActivity {
                     String retrieveUsername = dataSnapshot.child("name").getValue().toString();
                     String retrieveStatus = dataSnapshot.child("status").getValue().toString();
                     String retrieveProfileImage = dataSnapshot.child("image").getValue().toString();
-
-
                     myUser.setUserName(retrieveUsername);
                     myUser.setUserStatus("my turn");
-
-
 
                 } else if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("name"))) {
 
@@ -288,30 +246,22 @@ public class MainActivity extends AppCompatActivity {
 
                     if(myUser.getUserStatus().trim().equalsIgnoreCase("seeker"))
                     {
-
-                        Log.d(TAG, "onDataChange: "+ "We made it");
                         seekers = new SeekerTabsAdapter(getSupportFragmentManager());
-                        //myViewPager.setAdapter(myTabsAccessorAdapter);
                         myViewPager.setAdapter(seekers);
+                        myTabLayout = findViewById(R.id.main_tabs);
+                        myTabLayout.setupWithViewPager(myViewPager);
 
                     }else{
 
-                    myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
-                    myViewPager.setAdapter(myTabsAccessorAdapter);
+                        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+                        myViewPager.setAdapter(myTabsAccessorAdapter);
+                        myTabLayout = findViewById(R.id.main_tabs);
+                        myTabLayout.setupWithViewPager(myViewPager);
                     }
-
-
-
-                    myTabLayout = findViewById(R.id.main_tabs);
-                    myTabLayout.setupWithViewPager(myViewPager);
-
-
 
                 } else {
                     Toast.makeText(MainActivity.this, "Update Profile", Toast.LENGTH_SHORT).show();
                 }
-
-
             }
 
             @Override
@@ -320,7 +270,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //myUser.setUserStatus("my beats");
         Log.d(TAG, "myUser: "+myUser.getUserStatus());
         return myUser;
     }
