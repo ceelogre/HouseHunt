91aac8d4283fb1a487a950a474eec8783127d7ad
mugyenyij <mugyenyij@gmail.com>
2018-11-29 11:02:23 +0200

    Adding status spinner to project

---
+++ app/src/main/java/com/example/jmugyenyi/mychat/HouseHeadTabsAdapter.java
@@ -0,0 +1,60 @@
+package com.example.jmugyenyi.mychat;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * * @author Joel Mugyenyi
+ *
+ * Andrew ID: jmugyeny
+ *
+ * On my honor, as a Carnegie-Mellon Africa student, I have neither given nor
+ * received unauthorized assistance on this work.!
+ */
+public class HouseHeadTabsAdapter extends FragmentPagerAdapter {
+
+
+
+    public HouseHeadTabsAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i)
+        {
+            case 0:
+                ChatFragment chat = new ChatFragment();
+                return chat;
+            case 1:
+                PostAHouseFragment postedHouse = new PostAHouseFragment();
+                return postedHouse;
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position)
+        {
+            case 0:
+
+                return "Chat";
+            case 1:
+
+                return "Post a house";
+            default:
+                return null;
+        }
+    }
+}
